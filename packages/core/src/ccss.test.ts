36f30b4064e5865330d4eb437dc1f3e7ec3f1294
pablohenrique <pablo@softbox.com.br>
2020-01-10 19:24:24 +0100

    fix(core/ccss.test.ts): updated tests according to current development

--- packages/core/src/ccss.test.ts
+++ packages/core/src/ccss.test.ts
@@ -17,20 +17,18 @@ describe('ccss tests', () => {
             expect(ccss({ fd: 'c' })).toBe('flex-direction: column;')
         })
         it('parseSingle', () => {
-            expect(ccss({ ti: 2 })).toBe('text-indent: calc(2 * var(--px));')
+            expect(ccss({ ti: 2 })).toBe('text-indent: calc(2 * rem);')
         })
         it('parseMultipart', () => {
-            expect(ccss({ m: [1, 2, 3, 4] })).toBe(
-                'margin: calc(16 * var(--px)) calc(32 * var(--px)) calc(48 * var(--px)) calc(64 * var(--px)) ;'
-            )
-            expect(ccss({ m: [1, '1:3'] })).toBe('margin: calc(16 * var(--px)) 300% ;')
-            expect(ccss({ m: 10 })).toBe('margin: calc(10 * var(--px));')
-            expect(ccss({ m: '1:2' })).toBe('margin: 200%;')
-            expect(ccss({ m: ['1:2'] })).toBe('margin: 200% ;')
-            expect(ccss({ mt: '1:2' })).toBe('margin-top: 200%;')
+            expect(ccss({ m: [1, 2, 3, 4] })).toBe('margin: calc(1 * rem) calc(2 * rem) calc(3 * rem) calc(4 * rem) ;')
+            expect(ccss({ m: [1, '1:3'] })).toBe('margin: calc(1 * rem) 1:3 ;')
+            expect(ccss({ m: 10 })).toBe('margin: calc(10 * rem);')
+            expect(ccss({ m: '1:2' })).toBe('margin: 1:2;')
+            expect(ccss({ m: ['1:2'] })).toBe('margin: 1:2 ;')
+            expect(ccss({ mt: '1:2' })).toBe('margin-top: 1:2;')
         })
         it('pipe', () => {
-            expect(ccss({ r: 'global' })).toBe('border-radius: calc(6 * var(--px));')
+            expect(ccss({ r: 'global' })).toBe('border-radius: calc(6 * rem);')
         })
         it('undefined', () => {
             expect(ccss({ a: undefined })).toBe('')
