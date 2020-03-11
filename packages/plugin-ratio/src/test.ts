3a7f78da2b1ced479b3ccc933ad2500c8bd3ddd7
wintercounter <wintercounter@gmail.com>
2020-03-11 23:00:58 +0100

    fix: plugin updates after short name changes ... again

--- packages/plugin-ratio/src/test.ts
+++ packages/plugin-ratio/src/test.ts
@@ -11,13 +11,13 @@ describe('plugin-ratio tests', () => {
         it('parseSingle', () => {
             expect(ccss({ m: 10 })).toBe('margin: 10rem;')
             expect(ccss({ m: '1:2' })).toBe('margin: 200%;')
-            expect(ccss({ mt: '1:2' })).toBe('margin-top: 200%;')
+            expect(ccss({ mT: '1:2' })).toBe('margin-top: 200%;')
         })
 
         it('parseSingle:object', () => {
             expect(ccssObject({ m: 10 })).toStrictEqual({ margin: '10rem' })
             expect(ccssObject({ m: '1:2' })).toStrictEqual({ margin: '200%' })
-            expect(ccssObject({ mt: '1:2' })).toStrictEqual({ marginTop: '200%' })
+            expect(ccssObject({ mT: '1:2' })).toStrictEqual({ marginTop: '200%' })
         })
 
         it('parseMultipart', () => {
