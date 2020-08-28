a2d70cae084786547d55bb12776b985f0dd4e1da
wintercounter <wintercounter@gmail.com>
2020-08-28 11:47:03 +0200

    fix invalid try-catch

--- packages/styled/src/index.ts
+++ packages/styled/src/index.ts
@@ -70,7 +70,7 @@ export const createStyledCCSS = ({
                 Ui[tag].defaultProps = defaultProps
                 ccssd[tag] = tagged(tag)
                 ccssd[tag].defaultProps = defaultProps
-            } catch () {}
+            } catch {}
         }
     }
 
