7bbba60978bdc330be6a36a5189722ebb5388694
wintercounter <wintercounter@gmail.com>
2021-06-28 21:58:55 +0200

    fix: workaround for babel bug around type re-exports

--- packages/cryptic-css/core/src/index.ts
+++ packages/cryptic-css/core/src/index.ts
@@ -5,7 +5,7 @@ export * from './ccss'
 export * from './parsers'
 export * from './outputTransformers'
 // Needs to be exported separately for module augmentation to work correctly
-export { CCSSProps, CCSSOptions } from './types'
+export type { CCSSProps, CCSSOptions } from './types'
 export * from './types'
 
 export { ccss as default } from './ccss'
